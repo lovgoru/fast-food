b5f266182a2e0b8c352ac7783816781b81d3c630
lovgoru <gorup.lovro107@gmail.com>
2023-05-24 18:11:17 +0200

    Changed deploy link

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://fast-food.onrender.com"],
+    origin: ["http://localhost:3000", "https://fast-food-8a0f.onrender.com"],
   })
 );
 
